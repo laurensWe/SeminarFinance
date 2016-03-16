d2a07845689996d713a765fff8f3a2fc75b8ec19
Eva Janssens <EvaJanssens@ma-145-90-55-246.mobile-devices.eur.nl>
2016-03-16 13:13:02 +0100

    nu ook met pca

--- Spillover and DGC.py
+++ Spillover and DGC.py
@@ -12,14 +12,18 @@ import statsmodels.tsa.api as tsa
 import seaborn as sb
 import numpy as np
 from pandas.tools.plotting import autocorrelation_plot
+from matplotlib.mlab import PCA
+
 #%%read data 
 file1 = pd.read_excel('Equity growth rates_Fed_correct.xlsx')
 file1 = file1.set_index(file1.Time)
 mdata =file1[['S&LG', 'FG', 'DF', 'HH&NP', 'W', 'NFB']]
-#%% moving window 
+#%% initialize 
 #window length
-i = 50
+i = 100
+n = 1 #number of PC's we use (for PCA I measure)
 listje = ['S&LG', 'FG', 'DF', 'HH&NP', 'W', 'NFB']
+pcalist = ['1', '2', '3', '4', '5', '6']
 j = 0
 DGC = pd.Series(np.zeros(255), index=range(0,255))
 DGC = pd.DataFrame(data=DGC)
@@ -27,18 +31,26 @@ DGCi = pd.DataFrame(np.zeros((255, 6)), index=range(0,255), columns=listje)
 SI = pd.Series(np.zeros(255), index=range(0,255))
 SI = pd.DataFrame(data=SI)
 SIi = pd.DataFrame(np.zeros((255,6)), index=range(0,255), columns=listje)
+StabilityVAR = pd.Series(np.zeros(255), index=range(0,255))
+StabilityVAR = pd.DataFrame(data=StabilityVAR)
+PCAall = pd.DataFrame(np.zeros((255,6)), index=range(0,255), columns=pcalist)
 
+#%% full model
 mdata_full=mdata
 model_full = tsa.VAR(mdata)
-results_full = model_full.fit(4)  
+results_full = model_full.fit(5)  
 results_full.summary()
 results_full.irf(12).plot()
 results_full.resid.plot()
+results_full.is_stable(verbose=False)
+
+#%% moving window estimation
 while (j < 255-i):
     mdata1=mdata[j:(j+i)]
     model = tsa.VAR(mdata1)
-    results = model.fit(4)  
+    results = model.fit(5)  
     results.summary()
+    StabilityVAR.set_value(j+i,0,results.is_stable(verbose=False))
 # in this part we calculate the degree of granger causality
     DGC1 = 0
     N = 6.0
@@ -47,7 +59,7 @@ while (j < 255-i):
         for y in listje:
             if y not in x:
 # you test whether x granger causes y                
-                if 'fail to reject' in results.test_causality(y, x, kind='f')['conclusion']:
+                if 'fail to reject' in results.test_causality(y, x, kind='f', verbose=False)['conclusion']:
                     DGC1=DGC1+1
                     DGCihulp=DGCihulp+1
         DGCi.set_value(j+i, x, DGCihulp/5.0)          
@@ -77,14 +89,23 @@ while (j < 255-i):
     SI.set_value(j+i, 0, SpillOverIndex)
 #spillover index for 1 step ahead is a0,12^2+a0,21^2/trace(A0*A0') if 2 variables
 #maybe consider if we also want to look at more steps ahead?
+    # in this part we calculate the PCA interconnectedness measure
+    results = PCA(mdata1)
+    PCAall.set_value(j+i, ['1','2','3','4','5','6'], results.fracs)
+
+    
     j=j+1
 DGC.set_index(file1.Time)
 DGC[i:255].set_index(file1.Time[i:255]).plot()
 SI[i:255].set_index(file1.Time[i:255]).plot()
 DGCi[i:255].set_index(file1.Time[i:255]).plot()
 SIi[i:255].set_index(file1.Time[i:255]).plot()
+StabilityVAR[i:255].set_index(file1.Time[i:255]).plot()
+PCAall[i:255].set_index(file1.Time[i:255]).plot()
 #%%
 SI.to_excel('SpillOverIndex.xlsx')
 DGC.to_excel('DGC.xlsx')
 DGCi.to_excel('DGCi.xlsx')
 SIi.to_excel('SIi.xlsx')
+StabilityVAR.to_excel('StabilityVAR.xlsx')
+PCAall.to_excel('PCA interconnectedness.xlsx')
