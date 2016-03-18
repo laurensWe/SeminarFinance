cb33d09ac3303e1e7ba8e5d071ff3efe17b1922f
Sebastiaan Vermeulen <Sebastiaan Vermeulen>
2016-03-18 09:16:16 +0100

    vectorized

--- epidemicModel.py
+++ epidemicModel.py
@@ -6,15 +6,13 @@ Created on Fri Mar 11 14:50:22 2016
 
 """
 # preamble
-
-import pandas as pd
 import numpy as np
-
-# some mock-data
-I = pd.DataFrame({'x':{0:0,1:1,2:1,3:0}})
+import pandas as pd
+from scipy.stats import norm
+import multiproc
 
 # using Metropolis-Hastings sampling for mean calculation
-def doEpidemicModel(crisis_data, window_size, precision = 1e-3,starting_value=None,info=0):
+def doEpidemicModel(crisis_data,window_size,n_iter,starting_value=None,info=0):
     """
     Compute the expectation and variance of the bayes-estimator for the model 
     specified in our paper. The posterior distribution is sampled using a 
@@ -25,21 +23,25 @@ def doEpidemicModel(crisis_data, window_size, precision = 1e-3,starting_value=No
     Parameters
     ----------
     crisis_data : numpy matrix, array or dataframe
-        The data to optimize over, should be 0's and 1's stored in a matrix 
+        The data to optimize over, should be 0's and 1's stored in a array 
         with size ``T*N``, where ``N`` is the number of sectors and ``T`` is 
         the number of observations.
     window_size : scalar
         The size of the rolling window. Should be between ``T`` and ``N``.
     precision : scalar
         The average variance at which to stop iterating.
+    probability : scalar
+        The probability with which the precision is met.
     starting_value : scalar
-        The starting value of the parameters. defaults to a matrix of ones.
+        The starting value of the parameters. defaults to a random array.
         
     Returns
     -------
-    mean : numpy matrix
+    R0 : numpy array
+        the R0 of the data
+    mean : numpy array
         the parameters' sample mean 
-    variance : numpy matrix
+    variance : numpy array
         the parameters' sample variance. This is not a measure of the 
         parameters' significance! it just gives an impression of how precise
         the estimated mean is.
@@ -53,64 +55,45 @@ def doEpidemicModel(crisis_data, window_size, precision = 1e-3,starting_value=No
         raise ValueError('The window size should be between the number of se'+\
             'ctors and the number of observations.')
     if starting_value is None:
-        starting_value = np.matrix(np.ones(shape=(T-window_size,N,N)))
+        starting_value = np.random.uniform(size=(T-window_size,N,N))
     (t,n,m) = starting_value.shape       
-    if (t!=T) or (n!=m) or (n!=N):
+    if (t!=T-window_size) or (n!=m) or (n!=N):
         raise ValueError('The supplied starting parameter should have dimens'+\
-            'ions [#obs.]*[#sectors]*[n#sectors].')
-            
+            'ions [#obs.]*[#sectors]*[#sectors].')
     # initialize the model
-    model = epidemicModel(crisis_data,window_size,precision)
+    return epidemicModel(crisis_data,window_size).optimize(starting_value,n_iter,info)
+
+def R0(L):
+    # R0 is the sum of all infection rates
+    r = (np.sum(L,axis=2)-L.diagonal(axis1=1,axis2=2))/L.diagonal(axis1=1,axis2=2)
+    return r
     
-    # run the model
-    return model.run(starting_value,info)
+def precision(variance, probability,n_iter):
+    return np.max(variance)*norm.ppf(.5+probability/2)/np.sqrt(n_iter)
 
 class epidemicModel(object):
     
-    def __init__(self,I,window_size,precision):
+    def __init__(self,I,window_size):
         # initialize the class variables
         self.T,self.N = I.shape
-        self.I = np.matrix(I)
+        self.I = np.array(I).astype(np.bool)
         self.window_size = window_size
-        self.precision = precision
         self.shape = (self.T-self.window_size,self.N,self.N)
-
-    def run(self,starting_value,info):
-        # main program that tracks the different steps we wish to follow
-        self.prior = starting_value
-        mean, var, count = self.optimize(starting_value,info)
-        R0 = self.R0(mean,info)
-        return R0, mean, var, count
     
-    def R0(self,L,info):
-        # R0 is the sum of all infection rates
-        r = np.sum(L,axis=2)-L.diagonal(axis1=1,axis2=2)
-        if info>0:
-            print('done calculating R0')
-        return r
-    
-    def optimize(self,starting_value,info):
-        error = 1
-        counter = 0
+    def optimize(self,starting_value,n_iter,info):
+        counter = 1
         current = starting_value.copy()
         m1 = current.copy()
-        m2 = current.copy()**2
-        while (error > self.precision):
+        m2 = np.power(current,2)
+        while (counter < n_iter):
+            counter += 1
             # sample a new datapoint
             self.metropolis_hastings_sampler(current)
-            # update the 1st and 2nd moment and the error
+            # update the 1st and 2nd moment and the variance
             m1 += current
             m2 += np.power(current,2)
-            error = np.mean( (m2/counter-np.power(m1/counter,2))/counter )
-            if info>0:
-                print('counter: %12d, %12.6f'%(counter,error))
-                if info>1:
-                    print('first moment:\n',m1/counter)
-                    if info>2:
-                        print('second moment:\n',m2/counter)
-        if info>0:
-            print('done with optimization')            
-        return m1/counter, (m2/counter-np.power(m1/counter,2)), counter
+        # TODO return array of results
+        return m1/counter, m2/counter-np.power(m1/counter,2)
            
     def metropolis_hastings_sampler(self,current):
         """
@@ -123,44 +106,24 @@ class epidemicModel(object):
         proposal = np.random.uniform(0,1,size=self.shape)
         # the acceptance rate of the metropolis sampler for this iteration
         acceptance_rate = np.random.uniform()
-        # condition number is 1 because the uniform distribution is symmetric
-        condition_number = 1
-        # loop over all windows        
+        # loop over all windows   
+        p_i = proposal.diagonal(axis1=1,axis2=2)    
+        p_tildes = 1-np.prod(1-proposal, axis = 1)/(1-p_i)
+        
         for start in range(1,self.T-self.window_size):
-            probability = self.likelihood_per_window(start,proposal[start,:,:]) \
-                / self.likelihood_per_window(start,current[start,:,:]) 
+            probability = self.likelihood_per_window(start,p_i[start,:],p_tildes[start,:]) \
+                / self.likelihood_per_window(start,p_i[start,:],p_tildes[start,:]) 
             # if the proposal should be accepted, update current. else keep current.
-            if probability*condition_number > acceptance_rate:
-                current[start,:,:] = proposal[start,:,:].copy()
+            if probability > acceptance_rate:
+                current[start,:,:] = proposal[start,:,:]  
         
     # start may run from 1 to N-window_size
-    def likelihood_per_window(self,start,L):
-        # initialize the likelihood with the uniform prior
-        likelihood = 1
-        # nicely format the parameters
-        p = L.diagonal()
-        p_tildes = 1-np.prod(1-L+np.eye(self.N)*p,axis=0)   
-        # loop over all times in the window
-        for t in range(self.window_size):
-            likelihood *= self.likelihood_per_period(start+t,p,p_tildes)
-        return likelihood
-    
-    def likelihood_per_period(self,t,p,p_tildes):
-        likelihood = 1     
-        # loop over all sectors
-        for i in range(self.N):
-            likelihood *= self.p(self.I[t,i], self.I[t-1,i],  p[i], p_tildes[i])
-        return likelihood
-            
-    def p(i,i_lag,p_i,p_tilde):
-        # probability according to discrete (geometric) distribution
-        if (i_lag == 0):
-            if   (i == 1):
-                return p_tilde*(1-p_i)
-            else:# i = 0
-                return p_i*p_tilde + 1 - p_i
-        else: # i_lag = 1
-            if (i == 1):
-                return 1-p_i
-            else:# i = 0 
-                return p_i
+    def likelihood_per_window(self,start,p_i,p_tildes):
+        likelihood  = np.prod(self.I[start-1:start+self.window_size-1,:] \
+                    * np.repeat(p_i.reshape((1,p_i.size)),self.window_size,axis=0) \
+                    - self.I[start:start+self.window_size,:]) \
+                    * np.prod( ~self.I[start-1:start+self.window_size-1,:] \
+                    * np.repeat(1-p_i.reshape((1,p_i.size)),self.window_size,axis=0) \
+                    * np.repeat(p_tildes.reshape((1,p_tildes.size)),self.window_size,axis=0) \
+                    -  ~self.I[start:start+self.window_size,:])
+        return np.abs(likelihood)
