3e7535c7b183ab58dc600a3cbbae981127e60b6d
Sebastiaan Vermeulen <Sebastiaan Vermeulen>
2016-03-26 08:21:46 +0100

    moar moar moar, bijna compleet

--- epidemic model/Estimation of epidemic model.py
+++ epidemic model/Estimation of epidemic model.py
@@ -33,7 +33,7 @@ def doe_iets(df,window_size, period,n_iter):
     np.save(fname, results)
     
 if __name__ == '__main__':
-    for i in np.arange(len(df)-window_size,0,-1):
+    for i in [24,34]:#np.arange(122,145):
         doe_iets(df.iloc[i:i+window_size+1,:],window_size,i,n_iter)
 
 if False:			
