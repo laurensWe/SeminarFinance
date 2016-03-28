952032dd2b7e6e8c9a987e6a50ffc881de65f499
laurensWe <laurensweijs@hotmail.com>
2016-03-28 10:27:27 +0200

    Tie hie

--- PlotProgram.py
+++ PlotProgram.py
@@ -7,24 +7,25 @@ Created on Sat Mar 26 15:26:01 2016
 
 # preamble
 import numpy as np
-from scipy.stats import norm
-from matplotlib import pyplot,style
-style.use('ggplot')
 import pandas as pd
-import os
+from matplotlib import style,pyplot as plt
+from pandas.tools.plotting import autocorrelation_plot
+style.use('ggplot')
 
 wd = 'C:\\Users\\laure\\SharePoint\\Seminar - Documents\\Resultaten\\HHI en in-degree\\'
 
 hhi = pd.read_excel(wd + "hhi_6sectors.xlsx").plot()
-pyplot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 
 Indegree2 = pd.read_excel(wd + "temp2percent.xlsx").plot()
-pyplot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 pyplot.ylim(0,1.2)
 Indegree5 = pd.read_excel(wd + "temp5percent.xlsx").plot()
-pyplot.legend(loc='center left', bbox_to_anchor=(1, 0.5))
 pyplot.ylim(0,1.2)
 
+Systems = pd.read_excel(wd + "systemHHI_InDegree_6sectors.xlsx").plot()
+pyplot.ylim(0,1)
 
 #plot = ggplot(aes(x='Date', y='value', color='variable'), data=hhi) +geom_line()
 #fig = plot.draw()
+
+
+#%%
