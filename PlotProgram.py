1ecc6ff8f982eb7585076e382f2dd7f7f86d443d
laurensWe <laurensweijs@hotmail.com>
2016-03-28 14:14:28 +0200

    so mooi

--- PlotProgram.py
+++ PlotProgram.py
@@ -8,7 +8,7 @@ Created on Sat Mar 26 15:26:01 2016
 # preamble
 import numpy as np
 import pandas as pd
-from matplotlib import style,pyplot as plt
+from matplotlib import style,pyplot 
 from pandas.tools.plotting import autocorrelation_plot
 style.use('ggplot')
 
@@ -26,6 +26,13 @@ pyplot.ylim(0,1)
 
 #plot = ggplot(aes(x='Date', y='value', color='variable'), data=hhi) +geom_line()
 #fig = plot.draw()
+
+Systems1 = pd.read_excel(wd + "system1.xlsx").plot()
+pyplot.ylim(0,1)
+Systems2 = pd.read_excel(wd + "system2.xlsx").plot()
+pyplot.ylim(0,1)
+Systems3 = pd.read_excel(wd + "system3.xlsx").plot()
+pyplot.ylim(0,1)
 
 
 #%%
